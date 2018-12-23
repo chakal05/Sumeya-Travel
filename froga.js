0df5e20d1c33c03a34feacfba2d95f893a2f2fd2
chakal05 <chakirrasta@gmail.com>
2018-12-23 22:55:51 +0100

    adjustment

---
+++ froga.js
@@ -0,0 +1,27 @@
+
+    $(document).ready(function(){
+    shown();
+    $("#box").accordion({
+        collapsible: false
+        });
+          $(".ui-accordion-header").css("background","#1f1f1f") ;
+          $(".ui-accordion-header").css("color","#fff") ;
+          $(".ui-accordion-header.ui-state-active ").css("background","#1f1f1f") ;
+          
+    
+    })
+
+    function shown(){
+        $("#itoggle").click(function(){
+            $(".ligne1").hide();
+            $(".header ul li").show(hidden());
+        })
+    }
+    
+    function hidden(){
+        $("#itoggle").click(function(){
+            $(".ligne1").show();
+            $(".header ul li").hide(shown());
+        })
+    }
+   
