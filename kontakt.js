0ff1c053994d9004abda6c490a8be623e8bca8d4
chakal05 <chakirrasta@gmail.com>
2019-01-27 14:02:13 +0100

    kontakt page fixed queries for mobile

--- kontakt.js
+++ kontakt.js
@@ -1,18 +1,16 @@
 $(document).ready(function(){
+  shown();
+})
 
-    function hidden(){
-        $("#itoggle").click(function(){
-            $(".header ul li").hide(shown());
-        })
-    }
 
-    function shown(){
-        $("#itoggle").click(function(){
-            $(".header ul li").show(hidden());
-        })
-    }
+function hidden(){
+    $("#itoggle").click(function(){
+        $(".header ul li").hide(shown());
+    })
+}
 
+function shown(){
     $("#itoggle").click(function(){
         $(".header ul li").show(hidden());
     })
-})
+}
