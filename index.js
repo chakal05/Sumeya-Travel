0a8339f0f419a29b6df99b14d25f10b0603298ca
chakal05 <chakirrasta@gmail.com>
2019-02-14 13:11:59 +0100

    changes in index.js

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 $(document).ready(function(){
- shown();
- $( function() {
+ 
     $( "#datepicker" ).datepicker({
       changeMonth: true,
       changeYear: true
     });
-  } );
+  
 
-  $( function() {
+  
     $( "#datepickera" ).datepicker({
       changeMonth: true,
       changeYear: true
     });
-  } );
   
+    $("#itoggle").click(function(){
+      $(".header ul").show(hidden());
+      $(".header ul li").show(hidden());
+      $(".pub").hide();
+      $("#search").hide();
+  })
+
+  $("#itoggle").click(function(){
+    $(".header ul").hide(shown());
+    $(".header ul li").hide(shown());
+    $(".pub").show();
+    $("#search").show();
+})
 
 })
 
-
-function hidden(){
-    $("#itoggle").click(function(){
-        $(".header ul").hide(shown());
-        $(".header ul li").hide(shown());
-        $(".pub").show();
-        $("#search").show();
-    })
-}
-
-function shown(){
-    $("#itoggle").click(function(){
-        $(".header ul").show(hidden());
-        $(".header ul li").show(hidden());
-        $(".pub").hide();
-        $("#search").hide();
-    })
-}
