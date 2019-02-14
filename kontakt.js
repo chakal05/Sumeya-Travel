691dda59f158a0ff3fee4f242d0b266c9b113a5b
chakal05 <chakirrasta@gmail.com>
2019-02-14 13:18:09 +0100

    changes in kontakt.js

--- kontakt.js
+++ kontakt.js
@@ -1,16 +1,10 @@
 $(document).ready(function(){
-  shown();
-})
-
-
-function hidden(){
     $("#itoggle").click(function(){
         $(".header ul li").hide(shown());
     })
-}
 
-function shown(){
     $("#itoggle").click(function(){
         $(".header ul li").show(hidden());
     })
-}
+
+})
